a6e6224eb68298abde6e1dadf815c614d0d97882
toFindMore <17855824057@163.com>
2019-12-10 14:32:07 +0800

    HDU1003

--- HDU1003.cpp
+++ HDU1003.cpp
@@ -5,7 +5,7 @@
 
 int main() {
     int T;
-    int a[100005];
+    int a[100005], dp[100005];
     scanf("%d", &T);
     for (int i = 1; i <= T; i++) {
         int n;
@@ -13,10 +13,26 @@ int main() {
         for (int j = 0; j < n; j++) {
             scanf("%d", &a[j]);
         }
-
-
+        dp[0] = a[0];
+        int max = dp[0];
+        int left = 0, right = 0, sg = 0;
+        for (int j = 1; j < n; j++) {
+            dp[j] = dp[j - 1] >= 0 ? dp[j - 1] + a[j] : a[j];
+            if (dp[j - 1] < 0) {
+                sg = j;
+            }
+            if (max < dp[j]) {
+                max = dp[j];
+                left = sg;
+                right = j;
+            }
+        }
         printf("Case %d:\n", i);
-
+        printf("%d %d %d\n", max, left + 1, right + 1);
+        if (i != T) {
+            printf("\n");
+        }
     }
     return 0;
 }
+
