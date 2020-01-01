a1fc6cee6ed5923d80590c4e8983aae48b9ea848
toFindMore <17855824057@163.com>
2020-01-01 17:07:38 +0800

    feat:HDU1010 Tempter of the Bone

--- hdu/HDU1010.cpp
+++ hdu/HDU1010.cpp
@@ -2,11 +2,71 @@
 // Created by 周健 on 2019-12-30.
 //
 #include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+
+using namespace std;
+
+const int SIZE = 10;
+const int INF = 0x3ffffff;
+
+int n, m, t;
+int endI, endJ;
+int cnt[SIZE][SIZE];
+char maze[SIZE][SIZE];
+bool visit[SIZE][SIZE];
+bool flag;
+
+void travel(int i, int j, int step) {
+    // 不能继续下去的条件
+    if (step == -1 || i < 0 || i >= n || j < 0 || j >= m || maze[i][j] == 'X' || visit[i][j] || flag) {
+        return;
+    }
+    // 第step步刚好门开
+    if (maze[i][j] == 'D' && step == 0) {
+        flag = true;
+        return;
+    }
+
+    // 重要剪枝，否则会超时。x算出的是绝对最近距离
+    // x的奇偶性一定与step一致，否则在step步跳不到目标格子
+    int x = abs(endI - i) + abs(endJ - j);
+    if (x > step || (x & 1) ^ (step & 1)) {
+        return;
+    }
+
+    visit[i][j] = true;
+    travel(i + 1, j, step - 1);
+    travel(i - 1, j, step - 1);
+    travel(i, j + 1, step - 1);
+    travel(i, j - 1, step - 1);
+    visit[i][j] = false;
+}
 
 int main() {
-    int n, m, t;
-    while (scanf("%d%d%d", &n, &m, &t) && !(n == 0 && m == 0 && t == 0)) {
-
+    int startI, startJ;
+    while (scanf("%d%d%d", &n, &m, &t) && n || m || t) {
+        fill(visit[0], visit[0] + SIZE * SIZE, false);
+        fill(cnt[0], cnt[0] + SIZE * SIZE, INF);
+        flag = false;
+        startI = -1;
+        startJ = -1;
+        getchar();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                scanf("%c", &maze[i][j]);
+                if (maze[i][j] == 'S') {
+                    startI = i;
+                    startJ = j;
+                } else if (maze[i][j] == 'D') {
+                    endI = i;
+                    endJ = j;
+                }
+            }
+            getchar();
+        }
+        travel(startI, startJ, t);
+        flag ? printf("YES\n") : printf("NO\n");;
     }
     return 0;
 }
