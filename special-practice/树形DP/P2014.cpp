59966892a840a7d788fc293dade71ab81f2f8568
toFindMore <17855824057@163.com>
2020-01-07 10:24:55 +0800

    feat:P2014 选课【树形DP】

--- special-practice/树形DP/P2014.cpp
+++ special-practice/树形DP/P2014.cpp
@@ -3,6 +3,7 @@
 //
 #include <stdio.h>
 
+// 不能为0
 int tot;
 
 // 利用链式前向星进行数据存储
@@ -53,71 +54,5 @@ int main() {
     dfs(0);
     // 共有m+1个，因为虚拟节点也算一个
     printf("%d\n", dp[0][m + 1]);
-//    for(int i=0;i<20;i++) {
-//        printf("%d %d\n",edge[i].to,edge[i].next);
-//    }
     return 0;
 }
-
-//#include<iostream>
-//#include<cstdio>
-//#define maxn 1000
-//using namespace std;
-//int n,m,f[maxn][maxn],head[maxn],cnt;
-//struct edge
-//{
-//    int to,pre;
-//}e[maxn];
-//inline int in()
-//{
-//    char a=getchar();
-//    while(a<'0'||a>'9')
-//    {
-//        a=getchar();
-//    }
-//    int t=0;
-//    while(a>='0'&&a<='9')
-//    {
-//        t=(t<<1)+(t<<3)+a-'0';
-//        a=getchar();
-//    }
-//    return t;
-//}
-//void add(int from,int to)
-//{
-//    e[++cnt].pre=head[from];
-//    e[cnt].to=to;
-//    head[from]=cnt;
-//}
-//void dp(int now)
-//{
-////    f[now][0]=0;
-//    for(int i=head[now];i;i=e[i].pre)
-//    {
-//        int go=e[i].to;
-//        dp(go);
-//        for(int j=m+1;j>=1;j--)
-//        {
-//            for(int k=0;k<j;k++)
-//            {
-//                f[now][j]=max(f[now][j],f[go][k]+f[now][j-k]);
-//            }
-//        }
-//    }
-//}
-//int main()
-//{
-//    n=in(),m=in();
-//    for(int i=1;i<=n;i++)
-//    {
-//        int fa=in();
-//        f[i][1]=in();
-//        add(fa,i);
-//    }
-//    dp(0);
-//    printf("%d\n",f[0][m+1]);
-//    for(int i=0;i<20;i++) {
-//        printf("%d %d\n",e[i].to,e[i].pre);
-//    }
-//    return 0;
-//}
