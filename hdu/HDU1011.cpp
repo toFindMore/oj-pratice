44ad6a1ce0ebe7a212a3c0757272e2d4480fe049
toFindMore <17855824057@163.com>
2020-01-16 10:59:06 +0800

    feat: HDU1011 Starship Troopers

--- hdu/HDU1011.cpp
+++ hdu/HDU1011.cpp
@@ -1,4 +1,97 @@
 //
 // Created by 周健 on 2020-01-02.
 //
+#include <stdio.h>
+#include <cstring>
 
+const int SIZE = 105;
+
+struct Cost {
+    int bug;
+    int brain;
+} cost[SIZE];
+
+// 注意这边edge要开2倍大小，因为是双向边
+struct Node {
+    int to;
+    int next;
+} edge[SIZE * 2];
+
+int head[SIZE];
+
+int tot = 0;
+
+int dp[SIZE][SIZE];
+
+int visit[SIZE];
+
+int n, m;
+
+inline int max(int a, int b) {
+    return a > b ? a : b;
+}
+
+inline int ceil(int t) {
+    return t % 20 ? (t / 20 + 1) : t / 20;
+}
+
+// 链式前向星 存储树结构
+inline void add(int from, int to) {
+    edge[++tot].next = head[from];
+    edge[tot].to = to;
+    head[from] = tot;
+}
+
+void init() {
+    memset(edge, 0, sizeof(edge));
+    memset(head, 0, sizeof(head));
+    memset(visit, 0, sizeof(visit));
+    memset(dp, 0, sizeof(dp));
+    tot = 0;
+}
+
+void dfs(int now) {
+    // 表示访问过该点
+    visit[now] = 1;
+    // 一个房间打怪需要的士兵数目
+    int tmp = ceil(cost[now].bug);
+    // 如果拥有的士兵不够打，则获取不到经验
+    for (int i = m; i >= tmp; i--) {
+        dp[now][i] = cost[now].brain;
+    }
+    for (int i = head[now]; i; i = edge[i].next) {
+        int to = edge[i].to;
+        if (visit[to]) continue;
+        dfs(to);
+        for (int j = m; j >= tmp; j--) {
+            for (int k = 1; k <= j - tmp; k++) {
+                // j - k >= tmp
+                // j >= tmp
+                dp[now][j] = max(dp[now][j], dp[to][k] + dp[now][j - k]);
+            }
+        }
+    }
+}
+
+int main() {
+
+    while (scanf("%d%d", &n, &m) && !(n == -1 && m == -1)) {
+        init();
+        for (int i = 1; i <= n; i++) {
+            scanf("%d%d", &cost[i].bug, &cost[i].brain);
+        }
+        for (int i = 0; i < n - 1; i++) {
+            int n1, n2;
+            scanf("%d%d", &n1, &n2);
+            add(n1, n2);
+            add(n2, n1);
+        }
+        if (m == 0) {
+            printf("0\n");
+            continue;
+        }
+        dfs(1);
+        printf("%d\n", dp[1][m]);
+    }
+    return 0;
+}
--- readme.md
+++ readme.md
@@ -20,12 +20,14 @@
         - [1000(A+B)](http://acm.hdu.edu.cn/showproblem.php?pid=1000) [我的解法](https://github.com/toFindMore/OJ_Practice/blob/master/hdu/HDU1000.cpp)
         - [1001(区间和)](http://acm.hdu.edu.cn/showproblem.php?pid=1001) [我的解法](https://github.com/toFindMore/OJ_Practice/blob/master/hdu/HDU1001.cpp)
         - [1004(数最多个数)](http://acm.hdu.edu.cn/showproblem.php?pid=1004) [我的解法](https://github.com/toFindMore/OJ_Practice/blob/master/hdu/HDU1004.cpp)
-        - [1012(数最多个数)](http://acm.hdu.edu.cn/showproblem.php?pid=1012) [我的解法](https://github.com/toFindMore/OJ_Practice/blob/master/hdu/HDU1012.cpp)
+        - [1012(模拟计算)](http://acm.hdu.edu.cn/showproblem.php?pid=1012) [我的解法](https://github.com/toFindMore/OJ_Practice/blob/master/hdu/HDU1012.cpp)
+        - [1013(递归求数)](http://acm.hdu.edu.cn/showproblem.php?pid=1013) [我的解法](https://github.com/toFindMore/OJ_Practice/blob/master/hdu/HDU1013.cpp)
     - 模拟题
         - [1002(大数加法)](http://acm.hdu.edu.cn/showproblem.php?pid=1002) [我的解法](https://github.com/toFindMore/OJ_Practice/blob/master/hdu/HDU1002.cpp)
         - [1008(上下楼梯)](http://acm.hdu.edu.cn/showproblem.php?pid=1008) [我的解法](https://github.com/toFindMore/OJ_Practice/blob/master/hdu/HDU1008.cpp)
     - 动态规划
         - [1003(区间和最大)](http://acm.hdu.edu.cn/showproblem.php?pid=1003) [我的解法](https://github.com/toFindMore/OJ_Practice/blob/master/hdu/HDU1003.cpp)
+        - [1011(树形DP)](http://acm.hdu.edu.cn/showproblem.php?pid=1011) [我的解法](https://github.com/toFindMore/OJ_Practice/blob/master/hdu/HDU1011.cpp)
     - 快速幂：
         - [1005(矩阵快速幂)](http://acm.hdu.edu.cn/showproblem.php?pid=1005) [我的解法](https://github.com/toFindMore/OJ_Practice/blob/master/hdu/HDU1005.cpp)
     - 贪心：
